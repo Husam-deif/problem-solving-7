c18f72cf4715bc9510c992f02731258bfe5faae0
Husam-deif <husamdeif2005@gmail.com>
2026-01-31 01:52:41 +0300

    Add string and character input functions with vowel check
    Implement functions to read strings and characters, check for vowels, and solve the main logic.

---
+++ problem34.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
+string readString(string s) {
+	cout << s << endl;
+
+	string s2;
+	getline(cin, s2);
+
+	return s2;
+}
+char readChar(string s) {
+	cout << s << endl;
+	char s2;
+	cin >> s2;
+	return s2;
+}
+bool isVowels(char c1) {
+	c1 = tolower(c1);
+	if (c1 == 'i' || c1 == 'o' || c1 == 'u' || c1 == 'a' || c1 == 'e')
+		return true;
+	else
+		return false;
+}
+void solve() {
+	string s="";
+	int count = 0;
+	string s1 = readString("Enter string: ");
+	for (int i = 0; i < s1.length(); i++)
+	{
+		if (isVowels(s1[i]))
+			cout << s1[i] << "          ";
+	}
+	cout << endl;
+}
+int main() {
+	solve();
+}
